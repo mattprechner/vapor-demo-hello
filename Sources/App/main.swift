d79d63661bf79b35c1a9b6ab8bb98fbc23f50f58
Prechner, Matt (UK - London) <mprechner@deloitte.co.uk>
2017-04-27 15:38:20 +0100

    date/time

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,8 +5,18 @@ let drop = Droplet()
 
 drop.get("/") { request in
 
+	let now = Date()
+    let dayTimeFormatter = DateFormatter()
+    dayTimeFormatter.dateFormat = "E, d MMM yyyy"
+    let dayString = dayTimeFormatter.string(from: now)
+                
+    dayTimeFormatter.dateFormat = "HH:mm:ss Z"
+    let timeString = dayTimeFormatter.string(from: now)
+
 	return try JSON(node: [
-        "version": "1.0"
+        "date": dayString,
+        "time": timeString,
+        "message": "Hello, World!"
     ])
 }
 
