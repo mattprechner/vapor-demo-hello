d8f8dc7470254727b18954876dcc01ff1627584b
Prechner, Matt (UK - London) <mprechner@deloitte.co.uk>
2017-04-27 16:51:21 +0100

    Added params

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -20,4 +20,27 @@ drop.get("/") { request in
     ])
 }
 
+drop.get("/name",":name") { request in
+
+	let now = Date()
+    let dayTimeFormatter = DateFormatter()
+    dayTimeFormatter.dateFormat = "E, d MMM yyyy"
+    let dayString = dayTimeFormatter.string(from: now)
+                
+    dayTimeFormatter.dateFormat = "HH:mm:ss Z"
+    let timeString = dayTimeFormatter.string(from: now)
+	var message = "Hello, World!"
+    if let name = request.parameters["name"]?.string {
+        message = "Hello \(name)!"
+
+    }
+
+	return try JSON(node: [
+        "date": dayString,
+        "time": timeString,
+        "message": message
+    ])
+
+}
+
 drop.run()
